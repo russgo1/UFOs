86bba0943ea30e8c4dfb1eb00fc2b2d38a6711bf
russgo1 <russellg580@gmail.com>
2022-12-22 00:08:21 -0500

    Through 12.5

--- static/js/app.js
+++ static/js/app.js
@@ -20,3 +20,19 @@ function buildTable(data) {
         });
     });
 }
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+
+    let filteredData = tableData;
+
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    buildTable(filteredData)
+};
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+buildTable(tableData);
